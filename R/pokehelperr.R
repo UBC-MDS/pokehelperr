f532fc3f7c365c4be6f53d1cfb7abf9f330b563e
Jakob Thoms <j99thoms@gmail.com>
2023-01-15 03:58:51 -0800

    add source code file with skeleton for functions

---
+++ R/pokehelperr.R
@@ -0,0 +1,49 @@
+
+#' get_types
+#'
+#' @param pokemon_names
+#'
+#' @return
+#' @export
+#'
+#' @examples
+get_types <- function(pokemon_names){
+  # Function code (TBD in Milestone 3)
+}
+
+#' calc_resistances
+#'
+#' @param team_list
+#'
+#' @return
+#' @export
+#'
+#' @examples
+calc_resistances <- function(team_list){
+  # Function code (TBD)
+}
+
+#' calc_weaknesses
+#'
+#' @param team_list
+#'
+#' @return
+#' @export
+#'
+#' @examples
+calc_weaknesses <- function(team_list){
+  # Function code (TBD)
+}
+
+#' recommend
+#'
+#' @param current_team
+#'
+#' @return
+#' @export
+#'
+#' @examples
+recommend <- function(current_team){
+  # Function code (TBD)
+}
+
