1bc048085c3bd6c72cc7cef24e2256c559b0b80a
Jakob Thoms <j99thoms@gmail.com>
2023-01-28 18:02:49 -0800

    update tests for recommend function

--- tests/testthat/test-pokehelperr.R
+++ tests/testthat/test-pokehelperr.R
@@ -3,7 +3,7 @@ test_that("`recommend` returns the correct type of output.", {
   output <- recommend(
     c('Pikachu', 'Charizard'),
     n_recommendations = 1,
-    early_stop = False # Do a full run for one test. Early stop for the rest.
+    early_stop = FALSE # Do a full run for one test. Early stop for the rest.
   )
   expect_type(output, "character")
 
@@ -11,14 +11,17 @@ test_that("`recommend` returns the correct type of output.", {
   output <- recommend(
     c('Pikachu', 'Charizard'),
     n_recommendations = n,
-    early_stop = True
+    early_stop = TRUE
   )
   expect_type(output, "character")
   expect_equal(length(output), n)
-})
 
-test_that("`recommend` deals with errorneous user input in the case of a team with only one pokémon.", {
-  actual <- recommend('Pikachu', early_stop = True)
-  expected <- recommend(c('Pikachu'), early_stop = True)
-  expect_equal(actual, expected)
+  n <- 5
+  output <- recommend(
+    'Lucario',
+    n_recommendations = n,
+    early_stop = TRUE
+  )
+  expect_type(output, "character")
+  expect_equal(length(output), n)
 })
